a0d41eceef2f09fa7a312beef665a1e5df4d3e33
Kaysarul Anas Apurba <48248286+Anaskaysar@users.noreply.github.com>
2021-10-28 10:10:30 +0600

    up

--- index.js
+++ index.js
@@ -67,7 +67,9 @@ run().catch(console.dir);
 app.get('/',(req,res)=>{
     res.send("Running Genius Server");
 })
-
+app.get('/hello',(req,res)=>{
+    res.send("Hello updated here");
+})
 app.listen(port,()=>{
     console.log("Running Genius Server on Port",port)
 })
